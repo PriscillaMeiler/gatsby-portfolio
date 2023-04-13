1278245323efff8d21f7bda977ddf3a60ff0c852
Priscilla Meiler <priscilla.meiler@webcore.com.br>
2023-04-13 15:54:04 -0300

    Use State - Hide/Display Content

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React, { useState } from 'react';
 import { graphql, Link } from 'gatsby'
 import Layout from '../../components/layout'
 import Card from '../../components/postcard'
@@ -6,22 +6,46 @@ import Header from '../../components/header'
 import Seo from '../../components/seo'
 
 const BlogPage = ({ data }) => {
-  return (
-    <main>
-      <Header></Header>
-      <Layout pageTitle="Blog">
-      {
-        data.allMdx.nodes.map((node) => (
-          <Link to={`/blog/${node.frontmatter.slug}`} style={{textDecoration: 'none' }} key={node.id}>
-            <Card title={node.frontmatter.title} id={node.id} date={node.frontmatter.date}>
-              <p>{node.excerpt}</p>
-            </Card>
-          </Link>
-        ))
-      }
-      </Layout>
-    </main>
-  )
+  const [state, setState] = useState({
+    visible: true,
+  });
+  const displayPosts = () => {
+    setState({
+      ...state,
+      visible: !state.visible,
+    });
+  };
+
+  if(state.visible) {
+    return (
+      <main>
+        <Header></Header>
+        <Layout pageTitle="Blog">
+          <button data-state={state.scrolled} onClick={displayPosts}>Hide Posts</button>
+        {
+          data.allMdx.nodes.map((node) => (
+            <Link to={`/blog/${node.frontmatter.slug}`} style={{textDecoration: 'none' }} key={node.id}>
+              <Card title={node.frontmatter.title} id={node.id} date={node.frontmatter.date}>
+                <p>{node.excerpt}</p>
+              </Card>
+            </Link>
+          ))
+        }
+        </Layout>
+      </main>
+    )
+  }
+  else {
+    return (
+      <main>
+        <Header></Header>
+        <Layout pageTitle="Blog">
+          <button data-state={state.scrolled} onClick={displayPosts}>Show Posts</button>
+        </Layout>
+      </main>
+    )
+  }
+  
 }
 
 export const query = graphql`
